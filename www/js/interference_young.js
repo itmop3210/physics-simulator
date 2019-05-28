9639ddeeba668b13fa4aaa9fdb8958c4108b76d3
allacee <ddimaglushkov@ya.ru>
2019-05-28 23:12:48 +0300

    Logic realisation

--- www/js/interference_young.js
+++ www/js/interference_young.js
@@ -13,21 +13,27 @@ window.onload = function() {
     ctx.fillRect(0, 0, 400, 300);
 }
 
-
-function updateValues(){
-    lambda = document.getElementById("lambda").value;
-    hole_height = document.getElementById("hole_height").value;
-    source_width = document.getElementById("source_width").value;
-    screen_width = document.getElementById("screen_width").value;
-}
-
-
 function updateCanvasSize() {
     const canvas = document.getElementById('canvas');
 
     let size = Math.min(window.innerWidth, (window.innerHeight - TOP_ALLIGN) * 4 / 3  );
     canvas.style.width = size + 'px';
     canvas.style.height = size * 3 / 4  + 'px';
+}
+
+
+function updateValues(){
+    lambda = parseInt(document.getElementById("lambda").value);
+    hole_height = parseInt(document.getElementById("hole_height").value);
+    source_width = parseInt(document.getElementById("source_width").value);
+    screen_width = parseInt(document.getElementById("screen_width").value);
+}
+
+function calculateValues(){
+    updateValues();
+    let delta = screen_width / hole_height * lambda;
+    let x = (hole_height * (source_width + screen_width))/(2 * source_width);
+    alert(source_width + " " + hole_height / 2 + " " + (source_width + screen_width) + " " + x);
 }
 
 
